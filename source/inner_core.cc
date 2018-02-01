a3bb8a1e1f7ca9ae62d7201d9c52606caa0d24b0
MarineLasbleis <marine.lasbleis@gmail.com>
2018-02-01 10:16:29 +0900

    copy .cc files from cookbook IC to source, combing both of them.

--- source/inner_core.cc
+++ source/inner_core.cc
@@ -1,13 +1,13 @@
-#include <aspect/material_model/simple.h>
-#include <aspect/heating_model/interface.h>
-#include <deal.II/base/parsed_function.h>
 #include <aspect/simulator_access.h>
 #include <aspect/global.h>
 #include <aspect/simulator.h>
-#include <aspect/assembly.h>
+#include <aspect/simulator/assemblers/interface.h>
 
 #include <deal.II/base/quadrature_lib.h>
 #include <deal.II/fe/fe_values.h>
+
+
+
 
 namespace aspect
 {
@@ -19,6 +19,11 @@ namespace aspect
     class InnerCore : public MaterialModel::Simple<dim>
     {
       public:
+        /**
+         * Constructor.
+         */
+        InnerCore ();
+
         virtual void evaluate(const MaterialModel::MaterialModelInputs<dim> &in,
                               MaterialModel::MaterialModelOutputs<dim> &out) const;
 
@@ -58,6 +63,43 @@ namespace aspect
          * the model time).
          */
         Functions::ParsedFunction<dim> resistance_to_phase_change;
+
+      private:
+        /**
+         * Parameters related to the phase transition.
+         */
+        double transition_radius;
+        double transition_width;
+        double transition_temperature;
+        double transition_clapeyron_slope;
+        double transition_density_change;
+        bool compute_quadratic_pressure_profile;
+
+        /**
+         * A function object representing the hydrostatic pressure in the
+         * inner core as a function of radius. This is needed to compute
+         * the depth of phase transitions.
+         * Note that we can not simply use the adiabatic pressure here,
+         * as the equations are solved for the dynamic pressure, and the
+         * adiabatic conditions are used as initial guess for the solution.
+         */
+        Functions::ParsedFunction<1> hydrostatic_pressure_profile;
+
+        /**
+         * Percentage of material that has already undergone the phase
+         * transition to the higher-pressure material.
+         */
+        virtual
+        double
+        phase_function (const Point<dim> &position,
+                        const double temperature) const;
+
+        /**
+         * Hydrostatic pressure profile.
+         */
+        virtual
+        double
+        hydrostatic_pressure (const double radius) const;
     };
 
   }
@@ -67,6 +109,69 @@ namespace aspect
 {
   namespace MaterialModel
   {
+    template <int dim>
+    InnerCore<dim>::InnerCore ()
+      :
+      resistance_to_phase_change (1),
+      hydrostatic_pressure_profile (1)
+    {}
+
+    template <int dim>
+    double
+    InnerCore<dim>::
+    phase_function (const Point<dim> &position,
+                    const double temperature) const
+    {
+      // We need to convert the depth to pressure,
+      // keeping in mind that for this model, the pressure is only the dynamic pressure,
+      // so we have to get the hydrostatic pressure explicitly as an input.
+      const double radius = this->get_geometry_model().maximal_depth() - this->get_geometry_model().depth(position);
+      Assert(radius >= 0,
+             ExcMessage("There is a point in the model where the depth is larger "
+                        "than the maximal depth of this geometry."));
+
+      const double pressure_deviation = hydrostatic_pressure(radius) - hydrostatic_pressure(transition_radius);
+
+      double depth_deviation = transition_radius - radius;
+      if (std::fabs(pressure_deviation) > 100.0 * std::numeric_limits<double>::epsilon()
+          * (std::fabs(hydrostatic_pressure(radius)) + std::fabs(hydrostatic_pressure(transition_radius)))/2)
+        depth_deviation *= (1.0 - transition_clapeyron_slope * (temperature - transition_temperature) / pressure_deviation);
+
+      double phase_func;
+      // use delta function for width = 0
+      if (transition_width == 0)
+        phase_func = (depth_deviation > 0) ? 1 : 0;
+      else
+        phase_func = 0.5 * (1.0 + std::tanh(depth_deviation / transition_width));
+      return phase_func;
+    }
+
+
+    template <int dim>
+    double
+    InnerCore<dim>::
+    hydrostatic_pressure (const double radius) const
+    {
+      if (compute_quadratic_pressure_profile)
+        {
+          // Compute a quadratic hydrostatic pressure profile, based on a linear gravity model.
+          AssertThrow (dynamic_cast<const GravityModel::RadialLinear<dim>*>(&this->get_gravity_model())
+                       != 0,
+                       ExcMessage ("Automatic computation of the hydrostatic pressure profile is "
+                                   "only implemented for the 'radial linear' gravity model."));
+
+          const double max_radius = this->get_geometry_model().maximal_depth();
+          const Point<dim> surface_point = this->get_geometry_model().representative_point(0);
+          const double gravity_magnitude = this->get_gravity_model().gravity_vector(surface_point).norm();
+
+          // The gravity is zero in the center of the Earth, and we assume the density to be constant and equal to 1.
+          // We fix the surface pressure to 0 (and we are only interested in pressure differences anyway).
+          return gravity_magnitude * 0.5 * (1.0 - std::pow(radius/max_radius,2));
+        }
+      else
+        return hydrostatic_pressure_profile.value(Point<1>(radius));
+    }
+
 
     template <int dim>
     void
@@ -83,7 +188,8 @@ namespace aspect
       // density * cp to be 1
       for (unsigned int q=0; q < in.position.size(); ++q)
         {
-          out.densities[q] = - out.thermal_expansion_coefficients[q] * in.temperature[q];
+          out.densities[q] = - out.thermal_expansion_coefficients[q] * in.temperature[q]
+                             + phase_function (in.position[q], in.temperature[q]) * transition_density_change;
           if (std::abs(out.densities[q]) > 0.0)
             out.specific_heat[q] /= out.densities[q];
         }
@@ -100,6 +206,11 @@ namespace aspect
         resistance_to_phase_change.set_time (this->get_time() / year_in_seconds);
       else
         resistance_to_phase_change.set_time (this->get_time());
+
+      if (this->convert_output_to_years())
+        hydrostatic_pressure_profile.set_time (this->get_time() / year_in_seconds);
+      else
+        hydrostatic_pressure_profile.set_time (this->get_time());
     }
 
 
@@ -118,6 +229,53 @@ namespace aspect
             Functions::ParsedFunction<dim>::declare_parameters (prm, 1);
           }
           prm.leave_subsection();
+
+          prm.enter_subsection("Hydrostatic pressure function");
+          {
+            Functions::ParsedFunction<1>::declare_parameters (prm, 1);
+          }
+          prm.leave_subsection();
+
+          prm.declare_entry ("Phase transition radius", "0.0",
+                             Patterns::Double (0),
+                             "The distance from the center of the Earth where the phase "
+                             "transition occurs. "
+                             "Units: m.");
+          prm.declare_entry ("Phase transition width", "0.0",
+                             Patterns::Double (0),
+                             "The width of the phase transition. The argument of the phase function "
+                             "is scaled with this value, leading to a jump between phases "
+                             "for a value of zero and a gradual transition for larger values. "
+                             "Units: m.");
+          prm.declare_entry ("Phase transition temperature", "0.0",
+                             Patterns::Double (0),
+                             "The temperature at which the phase transition occurs in the depth "
+                             "given by the 'Phase transition depth' parameter.  Higher or lower "
+                             "temperatures lead to phase transition occurring in shallower or greater "
+                             "depths, depending on the Clapeyron slope given in 'Phase transition "
+                             "Clapeyron slope'. "
+                             "Units: K.");
+          prm.declare_entry ("Phase transition Clapeyron slope", "0.0",
+                             Patterns::Double (),
+                             "The Clapeyron slope of the phase transition. A positive "
+                             "Clapeyron slope indicates that the phase transition will occur in "
+                             "a greater depth if the temperature is higher than the one given in "
+                             "Phase transition temperatures (and in a smaller depth if the "
+                             "temperature is smaller than the one given in Phase transition temperatures). "
+                             "For negative Clapeyron slopes, the effect is in the opposite direction. "
+                             "Units: Pa/K.");
+          prm.declare_entry ("Phase transition density change", "0.0",
+                             Patterns::Double (),
+                             "The density change that occurs across the phase transition. "
+                             "A positive value means that the density increases with depth. "
+                             "Units: kg/m$^3$.");
+          prm.declare_entry ("Compute quadratic pressure profile from gravity", "true",
+                             Patterns::Bool (),
+                             "Whether to automatically compute the hydrostatic pressure profile "
+                             "(that is used to compute the location of phase transitions) from "
+                             "the magnitude of the gravity, assuming a linear gravity profile "
+                             "and a constant density (if true), or to use the function that is "
+                             "given in 'Hydrostatic pressure function' (if false).");
         }
         prm.leave_subsection();
       }
@@ -151,6 +309,30 @@ namespace aspect
               throw;
             }
           prm.leave_subsection();
+
+          prm.enter_subsection("Hydrostatic pressure function");
+          try
+            {
+              hydrostatic_pressure_profile.parse_parameters (prm);
+            }
+          catch (...)
+            {
+              std::cerr << "ERROR: FunctionParser failed to parse\n"
+                        << "\t'Hydrostatic pressure model.Function'\n"
+                        << "with expression\n"
+                        << "\t'" << prm.get("Function expression") << "'"
+                        << "More information about the cause of the parse error \n"
+                        << "is shown below.\n";
+              throw;
+            }
+          prm.leave_subsection();
+
+          transition_radius          = prm.get_double ("Phase transition radius");
+          transition_width           = prm.get_double ("Phase transition width");
+          transition_temperature     = prm.get_double ("Phase transition temperature");
+          transition_clapeyron_slope = prm.get_double ("Phase transition Clapeyron slope");
+          transition_density_change  = prm.get_double ("Phase transition density change");
+          compute_quadratic_pressure_profile = prm.get_bool ("Compute quadratic pressure profile from gravity");
         }
         prm.leave_subsection();
       }
@@ -275,6 +457,33 @@ namespace aspect
 }
 
 
+// explicit instantiations
+namespace aspect
+{
+  namespace MaterialModel
+  {
+    ASPECT_REGISTER_MATERIAL_MODEL(InnerCore,
+                                   "inner core material",
+                                   "A simple material model that is like the "
+                                   "'Simple' model, but has a constant $\rho c_p$, "
+                                   "and implements a function that characterizes the "
+                                   "resistance to melting/freezing at the inner core "
+                                   "boundary.")
+  }
+
+  namespace HeatingModel
+  {
+    ASPECT_REGISTER_HEATING_MODEL(ConstantCoreHeating,
+                                  "constant core heating",
+                                  "Implementation of a model in which the heating "
+                                  "rate is constant.")
+  }
+}
+
+
+
+
+
 namespace aspect
 {
   /**
@@ -306,7 +515,7 @@ namespace aspect
    */
   template <int dim>
   class PhaseBoundaryAssembler :
-    public aspect::internal::Assembly::Assemblers::AssemblerBase<dim>,
+    public aspect::Assemblers::Interface<dim>,
     public SimulatorAccess<dim>
   {
 
@@ -314,20 +523,21 @@ namespace aspect
 
       virtual
       void
-      phase_change_boundary_conditions (const typename DoFHandler<dim>::active_cell_iterator &cell,
-                                        const unsigned int                                    face_no,
-                                        internal::Assembly::Scratch::StokesSystem<dim>       &scratch,
-                                        internal::Assembly::CopyData::StokesSystem<dim>      &data) const
+      execute (internal::Assembly::Scratch::ScratchBase<dim>   &scratch_base,
+               internal::Assembly::CopyData::CopyDataBase<dim> &data_base) const
       {
+        internal::Assembly::Scratch::StokesSystem<dim> &scratch = dynamic_cast<internal::Assembly::Scratch::StokesSystem<dim>& > (scratch_base);
+        internal::Assembly::CopyData::StokesSystem<dim> &data = dynamic_cast<internal::Assembly::CopyData::StokesSystem<dim>& > (data_base);
+
         const Introspection<dim> &introspection = this->introspection();
-        const FiniteElement<dim> &fe = this->get_fe();
+        const FiniteElement<dim> &fe            = this->get_fe();
         const unsigned int stokes_dofs_per_cell = data.local_dof_indices.size();
         const unsigned int n_q_points           = scratch.face_finite_element_values.n_quadrature_points;
 
-        // assemble force terms for the matrix for all boundary faces
-        if (cell->face(face_no)->at_boundary())
+        //assemble force terms for the matrix for all boundary faces
+        if (scratch.cell->face(scratch.face_number)->at_boundary())
           {
-            scratch.face_finite_element_values.reinit (cell, face_no);
+            scratch.face_finite_element_values.reinit (scratch.cell, scratch.face_number);
 
             for (unsigned int q=0; q<n_q_points; ++q)
               {
@@ -365,31 +575,15 @@ namespace aspect
 
   template <int dim>
   void set_assemblers_phase_boundary(const SimulatorAccess<dim> &simulator_access,
-                                     internal::Assembly::AssemblerLists<dim> &assemblers,
-                                     std::vector<dealii::std_cxx11::shared_ptr<
-                                     internal::Assembly::Assemblers::AssemblerBase<dim> > > &assembler_objects)
+                                     Assemblers::Manager<dim> &assemblers)
   {
     AssertThrow (dynamic_cast<const MaterialModel::InnerCore<dim>*>
                  (&simulator_access.get_material_model()) != 0,
                  ExcMessage ("The phase boundary assembler can only be used with the "
                              "material model 'inner core material'!"));
 
-    std_cxx11::shared_ptr<PhaseBoundaryAssembler<dim> > phase_boundary_assembler
-    (new PhaseBoundaryAssembler<dim>());
-    assembler_objects.push_back (phase_boundary_assembler);
-
-    // add the terms for phase change boundary conditions
-    assemblers.local_assemble_stokes_system_on_boundary_face
-    .connect (std_cxx11::bind(&PhaseBoundaryAssembler<dim>::phase_change_boundary_conditions,
-                              std_cxx11::cref (*phase_boundary_assembler),
-                              std_cxx11::_1,
-                              std_cxx11::_2,
-                              // discard pressure_scaling,
-                              // discard rebuild_stokes_matrix,
-                              std_cxx11::_5,
-                              std_cxx11::_6));
-
-
+    PhaseBoundaryAssembler<dim> *phase_boundary_assembler = new PhaseBoundaryAssembler<dim>();
+    assemblers.stokes_system_on_boundary_face.push_back (std_cxx11::unique_ptr<PhaseBoundaryAssembler<dim> > (phase_boundary_assembler));
   }
 }
 
@@ -401,26 +595,3 @@ void signal_connector (aspect::SimulatorSignals<dim> &signals)
 
 ASPECT_REGISTER_SIGNALS_CONNECTOR(signal_connector<2>,
                                   signal_connector<3>)
-
-// explicit instantiations
-namespace aspect
-{
-  namespace MaterialModel
-  {
-    ASPECT_REGISTER_MATERIAL_MODEL(InnerCore,
-                                   "inner core material",
-                                   "A simple material model that is like the "
-                                   "'Simple' model, but has a constant $\rho c_p$, "
-                                   "and implements a function that characterizes the "
-                                   "resistance to melting/freezing at the inner core "
-                                   "boundary.")
-  }
-
-  namespace HeatingModel
-  {
-    ASPECT_REGISTER_HEATING_MODEL(ConstantCoreHeating,
-                                  "constant core heating",
-                                  "Implementation of a model in which the heating "
-                                  "rate is constant.")
-  }
-}
